e70b191e95bc372a6e24ddb74b62a788243c6d7b
knee-cola <nikola.derezic@gmail.com>
2019-03-10 21:03:47 +0100

    Updates

---
+++ precache-manifest.1416bfc5b9ac3fc9329b38bd038861bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38dcbaaa2f3d8f02892f",
+    "url": "/portal-penjanje/static/js/runtime~main.179d4f0d.js"
+  },
+  {
+    "revision": "6d48c42050656230b043",
+    "url": "/portal-penjanje/static/js/main.9cbe2bcf.chunk.js"
+  },
+  {
+    "revision": "7b81ba689e62078a325d",
+    "url": "/portal-penjanje/static/js/2.c331c3c4.chunk.js"
+  },
+  {
+    "revision": "6d48c42050656230b043",
+    "url": "/portal-penjanje/static/css/main.49ff760c.chunk.css"
+  },
+  {
+    "revision": "e5ab798b282fb64272e2d058198bc652",
+    "url": "/portal-penjanje/index.html"
+  }
+];
